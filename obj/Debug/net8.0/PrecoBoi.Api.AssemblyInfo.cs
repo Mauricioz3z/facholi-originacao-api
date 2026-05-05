99762dc49703366551193cb14baaf686cab48a1d
mauriciosouza-unoeste <maurcio.souza@unoeste.br>
2026-05-05 11:44:07 -0300

    ok

--- obj/Debug/net8.0/PrecoBoi.Api.AssemblyInfo.cs
+++ obj/Debug/net8.0/PrecoBoi.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrecoBoi.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e482ff882b337466068306c905b4650d42f333e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrecoBoi.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrecoBoi.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
